8caf26c45e55c408b02bbcc24a149c8f3e9c0a00
Tamu <thomas@platform5.ch>
2022-06-24 15:07:44 +0200

    Add compile condition

--- ConnectorTopSolid/UI/ConnectorBindingsTopSolid.cs
+++ ConnectorTopSolid/UI/ConnectorBindingsTopSolid.cs
@@ -174,8 +174,11 @@ namespace EPFL.SpeckleTopSolid.UI.LaunchCommand
         {
 
             var kit = KitManager.GetDefaultKit();
+#if (TOPSOLID715)
             var converter = kit.LoadConverter("TopSolid715");
-
+#else
+            var converter = kit.LoadConverter("TopSolid716");
+#endif
 
 
             if (state.Filter != null)
@@ -678,6 +681,6 @@ namespace EPFL.SpeckleTopSolid.UI.LaunchCommand
         /// <returns></returns>
         //public abstract List<ISelectionFilter> GetSelectionFilters();
 
-        #endregion
+#endregion
     }
 }

b8b1e4f3912e859d84baf1f176756ce550514a17
Tamu <thomas@platform5.ch>
2022-08-29 17:12:55 +0200

    Add ConvertibleObjects

--- ConnectorTopSolid/UI/Utils.cs
+++ ConnectorTopSolid/UI/Utils.cs
@@ -1,86 +1,12 @@
 ﻿using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
-using System.Drawing;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using TsUnits = TopSolid.Kernel.TX.Units.UnitFormat;
 using TopSolid.Kernel.DB.D3.Documents;
 using TopSolid.Kernel.DB.D3.Modeling.Documents;
-using TopSolid.Kernel.UI;
-using TopSolid.Kernel.DB.OptionSets;
 using TopSolid.Kernel.TX.Units;
-using TopSolid.Kernel.GR.Transforms;
-using TsApp = TopSolid.Kernel.UI.Application;
-
-
-
-namespace EPFL.SpeckleTopSolid.UIXXXX
-{
-    public partial class UtilsXXXX
-    {
-
-        public static string AppName = "TopSolid";
-#if TOPSOLID715
-        public static string TopSolidAppName = "TopSolid715"; // TODO: Update Speckle.Core.Kits
-#else
-        public static string TopSolidAppName = "TopSolid716"; // TODO: Update Speckle.Core.Kits
-#endif
-
-        #region units
-        private string _modelUnits;
-        public string ModelUnits
-        {
-            get
-            {
-
-                GeometricDocument Doc = TsApp.CurrentDocument as ModelingDocument;
-
-                if (string.IsNullOrEmpty(_modelUnits))
-                    _modelUnits = UnitToSpeckle(Doc.LengthUnit);
-                return _modelUnits;
-            }
-        }
-        private void SetUnits(Base geom)
-        {
-            geom["units"] = ModelUnits;
-        }
-
-        private double ScaleToNative(double value, string units)
-        {
-            var f = Units.GetConversionFactor(units, ModelUnits);
-            return value * f;
-        }
-
-        private string UnitToSpeckle(Unit units)
-        {
-
-            switch (units.Name) // TODO: Check Name conversion
-            {
-                case "Millimeter":
-                    return Units.Millimeters;
-                case "Centimeter":
-                    return Units.Centimeters;
-                case "Meter":
-                    return Units.Meters;
-                case "Kilometer":
-                    return Units.Kilometers;
-                case "Inche":
-                    return Units.Inches;
-                case "Fee":
-                    return Units.Feet;
-                case "Yard":
-                    return Units.Yards;
-                case "Mile":
-                    return Units.Miles;
-                default:
-                    throw new System.Exception("The current Unit System is unsupported.");
-            }
-        }
-        #endregion
-    }
-}
-
+using TopSolid.Kernel.DB.Elements;
 
 
 namespace Speckle.ConnectorTopSolid.UI
@@ -164,6 +90,26 @@ namespace Speckle.ConnectorTopSolid.UI
         {
             // remove ./
             return Regex.Replace(str, @"[./]", "-");
+        }
+
+        /// <summary>
+        /// Gets the handles of all visible document objects that can be converted to Speckle
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="converter"></param>
+        /// <returns></returns>
+        public static List<string> ConvertibleObjects(this ModelingDocument doc, ISpeckleConverter converter)
+        {
+            var objs = new List<string>();
+            IEnumerable<Element> elements = doc.Elements.GetAll();
+
+            foreach (Element element in elements)
+                {
+                    if (converter.CanConvertToSpeckle(element))
+                        objs.Add(element.Id.ToString());
+                }
+     
+            return objs;
         }
 
     }
